4edeb508950d49e872e848f598ee2b16da986be3
OpenShift guest <kfricilone@comcast.net>
2014-03-19 17:34:59 -0400

    Started on a scripting system

---
+++ src/net/ieldor/modules/scripts/Script.java
@@ -0,0 +1,7 @@
+package net.ieldor.modules.scripts;
+
+public abstract class Script {
+
+    public abstract void loadScripts(File path);
+    
+}
---
+++ src/net/ieldor/modules/scripts/impl/PythonScript.java
@@ -0,0 +1,12 @@
+package net.ieldor.modules.scripts.impl;
+
+import net.ieldor.modules.scripts.Script;
+
+public class PythonScript extends Scripts {
+
+    @Override
+    public void loadScripts(File path) {
+    
+    }
+
+}
